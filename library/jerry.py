84638c4ab586fd3aa9c71b96b955e9d4419a6b58
Jeronimo Garcia <garciaj.uk@gmail.com>
2016-09-16 07:11:40 -0400

    some work

---
+++ library/jerry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from ansible.module_utils.basic import *
+
+def main():
+    fields = { "testvar": { "required"  : True , "type" : "str"  }}
+    module = AnsibleModule(argument_spec=fields)
+    response = {"demo":[1,2,3,4]}
+    module.exit_json(changed=False, meta=response)
+
+
+if __name__ == '__main__':  
+    main()
